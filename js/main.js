05d1379c2a9aea0e8134c168d0199223c886ed14
vortec126 <diego.orobles@alumnos.udg.mx>
2018-02-20 20:06:48 -0600

    cagar carpetas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'area', { preload: preload, create: create, update: update });
-var platforms;
+
 function preload() {
 
     game.load.image('sky', 'assets/sky.png');
@@ -9,20 +9,25 @@ function preload() {
 
 }
 
+var platforms;
+
 function create() {
-    game.physics.startSystem(Phaser.physics.arcade);
+
+    //game.add.sprite(0, 0, 'star');
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     game.add.sprite(0, 0, 'sky');
     platforms = game.add.group();
-    platforms.enableBody=true;
+    platforms.enableBody = true;
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
+    ground.scale.setTo(2, 2);
+    ground.body.immovable = true;
 
-    var ground =platforms.create(0,game.world.height - 64,'ground');
-    ground.scale.setTo(2,2);
-    ground.body.inmovable=true;
+    var ledge= platforms.create(400, 400, 'ground');
 
-    var ledge =platforms.create(400,400,'ground');
-    ledge.body.inmovable=true;
-    ledge=plataforms.create(-150,250,'ground');
-    ledge.body.inmovable=true;
+    ledge.body.immovable = true;
+    ledge = platforms.create(-150, 250, 'ground');
+    ledge.body.immovable = true;
+
 
 }
 
